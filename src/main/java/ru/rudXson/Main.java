1024f965b00994abfa4150e16c46ed77312c785a
ruddnev <nikita-rudnev@ya,ru>
2023-02-17 02:20:19 +0300

    Initial commit. Now lets make it happen!

---
+++ src/main/java/ru/rudXson/Main.java
@@ -0,0 +1,7 @@
+package ru.rudXson;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initial commit. This will turn into a beautiful cli tool!");
+    }
+}

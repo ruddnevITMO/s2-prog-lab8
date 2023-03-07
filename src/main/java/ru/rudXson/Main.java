9560a3c8d91eabc2fbb3e42a1d6043c678c6a2f7
ruddnev <37450057+ruddnev@users.noreply.github.com>
2023-03-07 13:32:34 +0300

    adding Deserializer to Main and adding file validation

--- src/main/java/ru/rudXson/Main.java
+++ src/main/java/ru/rudXson/Main.java
@@ -1,10 +1,44 @@
 package ru.rudXson;
 
 import ru.rudXson.base.CommandExecutor;
+import ru.rudXson.base.Deserializer;
+import ru.rudXson.datatype.Flat;
+
+import ru.rudXson.base.FileValidator;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * Main method that is responsible for running the program.
+     *
+     * @param args Command line arguments that specify the name of the input file.
+     * @throws IOException If there is an error reading or writing to the file.
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String fileName;
+
+        // check if argument is provided
+        if (args.length < 1) {
+            System.out.println("No file name provided.");
+            System.out.print("Please enter file name: ");
+            fileName = scanner.nextLine();
+        } else {
+            fileName = args[0];
+        }
+
+        // Check if file exists and has write access
+        FileValidator.checkFile(fileName);
+
+        // Deserialize the file and store the data in a priority queue
+        PriorityQueue<Flat> flats = Deserializer.deserialize(fileName);
+
+        System.out.println(flats);
+
+        // Execute the interactive command line mode using CommandExecutor
         CommandExecutor go = new CommandExecutor();
-        go.StartInteractiveMode();
+        go.StartInteractiveMode(scanner);
     }
 }

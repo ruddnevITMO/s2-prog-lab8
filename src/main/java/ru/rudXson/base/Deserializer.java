9438659f4bcf52a75b43d7c24d785e494500fd20
ruddnev <37450057+ruddnev@users.noreply.github.com>
2023-03-07 13:31:33 +0300

    first version of the Deserializer

---
+++ src/main/java/ru/rudXson/base/Deserializer.java
@@ -0,0 +1,27 @@
+package ru.rudXson.base;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import com.google.gson.Gson;
+import ru.rudXson.datatype.Flat;
+
+public class Deserializer {
+
+    public static PriorityQueue<Flat> deserialize(String fileName) throws IOException {
+        Gson gson = new Gson();
+        PriorityQueue<Flat> queue = new PriorityQueue<>();
+
+        try (FileReader reader = new FileReader(fileName)) {
+            Flat[] flats = gson.fromJson(reader, Flat[].class);
+            for (Flat flat : flats) {
+                queue.offer(flat);
+            }
+        } catch (IOException e) {
+            throw new IOException("Failed to deserialize the priority queue from file: " + fileName, e);
+        }
+
+        return queue;
+    }
+
+}

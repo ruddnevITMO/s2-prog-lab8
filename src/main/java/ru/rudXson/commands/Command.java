c999707531a9be827b7772663a63bb6c49ce2181
ruddnev <37450057+ruddnev@users.noreply.github.com>
2023-04-13 16:00:16 +0300

    small beauty fixes

--- src/main/java/ru/rudXson/commands/Command.java
+++ src/main/java/ru/rudXson/commands/Command.java
@@ -1,10 +1,12 @@
 package ru.rudXson.commands;
 
 import ru.rudXson.exceptions.NoPermission;
+import ru.rudXson.exceptions.NotEnoughArgsException;
+import ru.rudXson.exceptions.WrongArgsException;
 
 import java.io.IOException;
 
 public interface Command {
-    public void execute(String [] args) throws NoPermission, IOException; // throws NEA, WA
+    public void execute(String [] args) throws NoPermission, IOException, WrongArgsException, NotEnoughArgsException; // throws NEA, WA
     public String getDescription();
 }

23135ee2aa042aab3173753d565d011cf48d12d4
ruddnev <37450057+ruddnev@users.noreply.github.com>
2023-05-01 03:49:34 +0300

    creating ExitException to break the program cycle and exit properly

--- src/main/java/ru/rudXson/commands/Exit.java
+++ src/main/java/ru/rudXson/commands/Exit.java
@@ -1,13 +1,15 @@
 package ru.rudXson.commands;
 
+import ru.rudXson.exceptions.ExitException;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Exit implements Command {
     @Override
-    public void execute(String[] args, boolean fromExecute, Scanner executeScanner) throws IOException {
+    public void execute(String[] args, boolean fromExecute, Scanner executeScanner) throws IOException, ExitException {
         System.out.println("Closed the program.");
-        System.exit(0);
+        throw new ExitException();
     }
 
     @Override

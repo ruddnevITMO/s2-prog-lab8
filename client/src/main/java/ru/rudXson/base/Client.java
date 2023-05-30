4ee845d4adddf3ff080b1501f17d645284e9f61d
ruddnev <37450057+ruddnev@users.noreply.github.com>
2023-05-30 21:32:16 +0300

    add Client connection functionality

---
+++ client/src/main/java/ru/rudXson/base/Client.java
@@ -0,0 +1,79 @@
+package ru.rudXson.base;
+
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.util.Arrays;
+import com.google.common.primitives.Bytes;
+import org.apache.commons.lang3.SerializationUtils;
+import ru.rudXson.requests.Request;
+import ru.rudXson.responses.Response;
+
+
+public class Client {
+    private final int PACKET_SIZE = 1024;
+
+    private final DatagramChannel client;
+    private final InetSocketAddress addr;
+
+    public Client(InetAddress address, int port) throws IOException {
+        this.addr = new InetSocketAddress(address, port);
+        this.client = DatagramChannel.open().bind(null).connect(addr);
+        this.client.configureBlocking(false);
+    }
+
+    public Response sendRequestGetResponse(Request request) throws IOException {
+        sendRequest(SerializationUtils.serialize(request));
+        return SerializationUtils.deserialize(getResponse());
+    }
+
+    private void sendRequest(byte[] data) throws IOException {
+        int DATA_SIZE = PACKET_SIZE - 1;
+        byte[][] ret = new byte[(int) Math.ceil(data.length / (double) DATA_SIZE)][DATA_SIZE];
+
+        int start = 0;
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = Arrays.copyOfRange(data, start, start + DATA_SIZE);
+            start += DATA_SIZE;
+        }
+
+
+        for (int i = 0; i < ret.length; i++) {
+            var chunk = ret[i];
+            if (i == ret.length - 1) {
+                var lastChunk = Bytes.concat(chunk, new byte[]{1});
+                client.send(ByteBuffer.wrap(lastChunk), addr);
+            } else {
+                var answer = Bytes.concat(chunk, new byte[]{0});
+                client.send(ByteBuffer.wrap(answer), addr);
+            }
+        }
+
+    }
+
+    private byte[] getResponse() throws IOException {
+        var received = false;
+        var result = new byte[0];
+
+        while (!received) {
+            var buffer = ByteBuffer.allocate(PACKET_SIZE);
+            SocketAddress address = null;
+            while (address == null) {
+                address = client.receive(buffer);
+            }
+            var data = buffer.array();
+
+            if (data[data.length - 1] == 1) {
+                received = true;
+            }
+            result = Bytes.concat(result, Arrays.copyOf(data, data.length - 1));
+        }
+
+        return result;
+    }
+
+}

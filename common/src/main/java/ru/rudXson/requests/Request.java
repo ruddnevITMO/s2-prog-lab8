c3576bf5c87a85ad5cdb9d63181c8ed9ec456784
ruddnev <37450057+ruddnev@users.noreply.github.com>
2023-06-12 03:36:38 +0300

    make every Request hold credentials

--- common/src/main/java/ru/rudXson/requests/Request.java
+++ common/src/main/java/ru/rudXson/requests/Request.java
@@ -4,8 +4,23 @@ import java.io.Serializable;
 
 public abstract class Request implements Serializable {
     public final String name;
+    private String username;
+    private String password;
 
     public Request(String name) {
         this.name = name;
+    }
+
+    public void setCreds(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
     }
 }
